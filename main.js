31bfede799af8bd68c37b22cf3cd62a627d205db
psalmskiddy <49799322+psalmskiddy@users.noreply.github.com>
2026-01-08 21:09:35 +0100

    Add files via upload
    initial website upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".navigate a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 120;
+        const sectionHeight = section.offsetHeight;
+
+        if (pageYOffset >= sectionTop &&
+            pageYOffset < sectionTop + sectionHeight) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === `#${current}`) {
+            link.classList.add("active");
+        }
+    });
+});
+const menuToggle = document.getElementById("menu-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
+
